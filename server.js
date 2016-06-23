7338d4e5a2329096772c5369635c0b3b803b0f9c
Norbert <noobertlm@gmail.com>
2016-06-22 23:20:19 -0400

    spacing

--- server.js
+++ server.js
@@ -22,87 +22,85 @@ db.once('open', function(){
 var router = express.Router();
 
 router.use(function(req, res, next){
-	console.log('Something');
-	next(); //Go to next route
+  console.log('Something');
+  next(); //Go to next route
 });
 
 // Routes ending in /books
 router.get('/', function(req, res){
-	res.json({message: 'welcome it works'});
+  res.json({message: 'welcome it works'});
 });
 
 
 router.route('/books') //Post and get book list
 
-	.post(function(req, res) {
+  .post(function(req, res) {
 
-		var book = new Book();
-		book.title = req.body.title;
-		book.author = req.body.author;
-		book.currentPage = req.body.currentPage;
-		book.totalPages = req.body.totalPages;
+    var book = new Book();
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.currentPage = req.body.currentPage;
+    book.totalPages = req.body.totalPages;
 
-		book.save((err)=>{
-			if(err){
-				res.send(err);
-			}
-			res.json({message: 'Book added'});
-			console.log('Book added');
-		});
-	})
+    book.save((err)=>{
+        if(err){
+            res.send(err);
+        }
+        res.json({message: 'Book added'});
+        console.log('Book added');
+    });
+})
 
-	.get((req, res)=>{
-		Book.find((err, books)=>{
-			if(err){
-				res.send(err);
-			}
-			res.json(books);
-		});
-	});
+  .get((req, res)=>{
+    Book.find((err, books)=>{
+        if(err){
+            res.send(err);
+        }
+        res.json(books);
+    });
+});
 
 
 // Routes ending in /books/:book_id
 router.route('/books/:book_id')
-	
-	.get((req, res)=>{ //Get book based on id
-		Book.findById(req.params.book_id, (err, book)=>{
-			if(err){
-				res.send(err);
-			}
-			res.json(book);
-		});
-	})
+    
+  .get((req, res)=>{ //Get book based on id
+    Book.findById(req.params.book_id, (err, book)=>{
+        if(err){
+            res.send(err);
+        }
+        res.json(book);
+    });
+})
 
-	.delete((req, res)=>{ //Delete book
-		Book.remove({
-			_id: req.params.book_id
-		}, (err, book)=> {
-			if(err) res.send(err);
+  .delete((req, res)=>{ //Delete book
+    Book.remove({
+        _id: req.params.book_id
+    }, (err, book)=> {
+        if(err) res.send(err);
+         res.json({ message: 'Deleted book'});
+    });
+})
 
-			res.json({ message: 'Deleted book'});
-		});
-	})
+  .put((req, res)=> { //Edit book information
+    Book.findById(req.params.book_id, (err, book)=>{
+        if(err){
+            res.send(err);
+        }
 
-	.put((req, res)=> { //Edit book information
-		Book.findById(req.params.book_id, (err, book)=>{
-			if(err){
-				res.send(err);
-			}
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.currentPage = req.body.currentPage;
+        book.totalPages = req.body.totalPages;
 
-			book.title = req.body.title;
-			book.author = req.body.author;
-			book.currentPage = req.body.currentPage;
-			book.totalPages = req.body.totalPages;
-
-			book.save((err)=>{
-				if(err){
-					res.send(err);
-				}
-
-				res.json({message: 'Updated book'});
-			});
-		});
-	});
+        book.save((err)=>{
+            if(err){
+                res.send(err);
+            }
+            res.json({message: 'Updated book'});
+        });
+    });
+});
 
 app.use('/api', router);
 
